e9488993c97b5d8a651fd2834c010489876819a2
beppek <beppe@live.se>
2016-11-25 16:09:50 +0100

    green km to miles

--- app/model/UnitConverter.js
+++ app/model/UnitConverter.js
@@ -9,7 +9,10 @@ function UnitConverter() {
 }
 
 UnitConverter.prototype.kmToMiles = function(km) {
-
+    var ratio = 0.62137;
+    var numbers = [km, ratio];
+    var miles = c.multiply(numbers).toFixed(5);
+    return miles;
 };
 
 UnitConverter.prototype.milesToKm = function(miles) {
