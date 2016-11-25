d98e6d8823390448c1edeb0c4be03d7058117f97
beppek <beppe@live.se>
2016-11-25 12:53:45 +0100

    refactor type error check

--- app/model/Calculator.js
+++ app/model/Calculator.js
@@ -6,21 +6,15 @@ function Calculator() {
 
 Calculator.prototype.add = function(numbers) {
     var result = 0;
+    checkInputAreNumbers(numbers);
     numbers.forEach(function(num) {
-        if (isNaN(num)) {
-            throw new TypeError("Input was not a number");
-        }
         result += num;
     });
     return result;
 };
 
 Calculator.prototype.subtract = function(numbers) {
-    numbers.forEach(function(num) {
-        if (isNaN(num)) {
-            throw new TypeError("Input was not a number");
-        }
-    });
+    checkInputAreNumbers(numbers);
     var result = numbers[0];
     var i;
     for (i = 1; i < numbers.length; i += 1) {
@@ -36,5 +30,13 @@ Calculator.prototype.multiply = function(a, b) {
 Calculator.prototype.divide = function(a, b) {
 
 };
+
+function checkInputAreNumbers(numbers) {
+    numbers.forEach(function(num) {
+        if (isNaN(num)) {
+            throw new TypeError("Input was not a number");
+        }
+    });
+}
 
 module.exports = Calculator;
