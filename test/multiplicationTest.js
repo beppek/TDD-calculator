9edc316bf4b96bbfad428abcb4f10c04521a32d2
beppek <beppe@live.se>
2016-11-25 15:08:13 +0100

    red type error multiply

--- test/multiplicationTest.js
+++ test/multiplicationTest.js
@@ -11,12 +11,16 @@ describe('Calculator', function () {
 
         var sut = new Calculator();
 
-
         it('should multiply 2 numbers', function () {
             var numbers = [2,3];
             var expected = 6;
             var actual = sut.multiply(numbers);
             assert.equal(expected, actual);
+        });
+
+        it('should throw TypeError if params are NaN', function () {
+            var notNumbers = ['a', 'b', 'c'];
+            assert.throws(function() {sut.multiply(notNumbers)}, TypeError, "Input was not a number");
         });
 
     });
