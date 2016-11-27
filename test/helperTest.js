960f192055172ca04efd9d560814e1d56b74c93b
beppek <beppe@live.se>
2016-11-27 10:37:46 +0100

    straight to green tests for numbers helper

--- test/helperTest.js
+++ test/helperTest.js
@@ -9,7 +9,22 @@ describe('Numbers helper', function () {
 
     it('should throw TypeError if input is not a number', function () {
         var notNumbers = ['a', 'b'];
-        assert.throws(function() {sut.checkAreNumbers(notNumbers)}, Error, "Input was not a number")
+        assert.throws(function() {sut.checkAreNumbers(notNumbers)}, TypeError, "Input was not a number")
+    });
+
+    it('should not throw any error if input is numbers', function () {
+        var numbers = [1,2];
+        assert.doesNotThrow(function() {sut.checkAreNumbers(numbers)});
+    });
+
+    it('should throw error if input is negative', function () {
+        var negativeNumbers = [-1,-2];
+        assert.throws(function() {sut.checkPositive(negativeNumbers)}, Error, "Input can't be a negative number");
+    });
+
+    it('should not throw error if numbers are positive', function () {
+        var numbers = [1,2];
+        assert.doesNotThrow(function() {sut.checkPositive(numbers)});
     });
 
 });
