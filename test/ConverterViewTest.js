112edebb0cc3f240cbd442322fd34c655122bc56
beppek <beppe@live.se>
2016-12-01 12:52:09 +0100

    red view miles to km

--- test/ConverterViewTest.js
+++ test/ConverterViewTest.js
@@ -85,6 +85,17 @@ describe('ConverterView', function () {
         assert.equal(actual, expected);
     });
 
+    it('should return correct ratio for miles to km', function () {
+        createHTML();
+        var dUnit1 = document.getElementById('distanceUnit1').children[2];
+        var dUnit2 = document.getElementById('distanceUnit2').children[1];
+        dUnit1.selected = true;
+        dUnit2.selected = true;
+        var expected = 1.609344;
+        var actual = sut.getRatio();
+        assert.equal(actual, expected);
+    });
+
 
     function createHTML() {
         var html =  '<div id="app">' +
